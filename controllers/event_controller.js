2d8a46ecd5f86eba63f51e6e9fa97c370482b35a
Ryan Foote <ryanfoote@Ryans-MacBook-Pro.local>
2017-08-21 00:35:44 -0400

    def need to flesh this out more, and define params

---
+++ controllers/event_controller.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var router = express.Router();
+//REMEMBER TO EDIT DB NAME
+var db = require("../models/");
+
+//logic for generating random PIN
+function joinEvent (
+    if (host = TRUE){
+        var PIN = Math.floor(100000 + Math.random() * 900000);
+        //post route to create in sql
+        router.post("/event/create", function(req, res) {
+            db.Event.create({
+            event_pin: req.body.event_pin
+            })
+            //pass the result of our call
+            .then(function(dbEvent) {
+            console.log(dbEvent);
+            res.redirect("/event/" + PIN);
+            });
+        });
+    }
+    else {
+        //note: we will need to define the guestPIN as user input on the front page
+        res.redirect("/event/"+ PIN);
+    };
+);
+
+//Need to add aspects that allow user input like a jquery button click for the events or join as a guest
+joinEvent();
