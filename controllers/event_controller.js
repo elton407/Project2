34be3b359d13ffc69d8d03f6aaf4aeddeb2648f6
Ryan Foote <ryanfoote@Ryans-MacBook-Pro.local>
2017-08-23 21:43:55 -0400

    Changing a few structure things as well

--- controllers/event_controller.js
+++ controllers/event_controller.js
@@ -4,26 +4,35 @@ var router = express.Router();
 var db = require("../models/");
 
 //logic for generating random PIN
-function joinEvent (
-    if (host = TRUE){
-        var PIN = Math.floor(100000 + Math.random() * 900000);
-        //post route to create in sql
-        router.post("/event/create", function(req, res) {
-            db.Event.create({
-            event_pin: req.body.event_pin
-            })
-            //pass the result of our call
-            .then(function(dbEvent) {
-            console.log(dbEvent);
-            res.redirect("/event/" + PIN);
-            });
-        });
-    }
-    else {
-        //note: we will need to define the guestPIN as user input on the front page
-        res.redirect("/event/"+ PIN);
-    };
-);
+// function joinEvent (
+//     if (host = TRUE){
+//         var PIN = Math.floor(100000 + Math.random() * 900000);
+//         //post route to create in sql
+//         router.post("/event/create", function(req, res) {
+//             db.Event.create({
+//             PINvitation: req.body.PINvitation
+//             })
+//             //pass the result of our call
+//             .then(function(dbEvent) {
+//             console.log(dbEvent);
+//             res.redirect("/event/" + PIN);
+//             });
+//         });
+//     }
+//     else {
+//         //note: we will need to define the guestPIN as user input on the front page
+//         res.redirect("/event/"+ PIN);
+//     };
+// );
 
-//Need to add aspects that allow user input like a jquery button click for the events or join as a guest
-joinEvent();
+// //Need to add aspects that allow user input like a jquery button click for the events or join as a guest
+// joinEvent();
+
+
+//////////////////4 reelz///////////////////////////////
+
+if (host) {
+    
+} else {
+    
+}
