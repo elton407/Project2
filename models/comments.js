982dc39e6a9632cad07806980d5f1b49fa317edb
Marcy <mlsantoyo@att.net>
2017-08-29 16:38:56 -0400

    added JAWS_DB

--- models/comments.js
+++ models/comments.js
@@ -1,28 +1,47 @@
-module.exports = function (sequelize, DataTypes) {
-    var Comments = sequelize.define("Comments", {
-        comment_body: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        }
-    });
+// module.exports = function (sequelize, DataTypes) {
+//     var Comments = sequelize.define("Comments", {
+//         comment_body: {
+//             type: DataTypes.STRING,
+//             allowNull: true
+
+            
+//         }
+//     });
 
     
-    Comments.associate = function (models) {
-        // Associating user with Posts
-        // When a user is deleted, also delete any associated Posts
-        Comments.belongsTo(models.Users, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-        Comments.belongsTo(models.Event, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
-    }
+//     // Comments.associate = function (models) {
+//     //     // Associating user with Posts
+//     //     // When a user is deleted, also delete any associated Posts
+//     //     Comments.belongsTo(models.Users, {
+//     //         foreignKey: {
+//     //             allowNull: false
+//     //         }
+//     //     });
+//     //     Comments.belongsTo(models.Event, {
+//     //         foreignKey: {
+//     //             allowNull: false
+//     //         }
+//     //     });
+//     // }
+//     return Comments;
+// };
+
+module.exports = function (sequelize, DataTypes) {
+    var Comments = sequelize.define("Comments", {
+       
+        comment_body: {
+            type: DataTypes.STRING
+        }
+
+      
+    });
+
+    // Event.associate = function (models) {
+    //     // Associating Author with Posts
+    //     // When an Author is deleted, also delete any associated Posts
+    //     Event.belongsTo(models.Users);
+    // };
+
     return Comments;
-};
+
+}; //closes the entire modal 
